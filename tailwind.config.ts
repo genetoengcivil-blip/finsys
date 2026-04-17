153e632061c599360c26f30de532d140285ff31e
Geraldo Neto <gnetoengcivil@hotmail.com>
2026-04-17 00:16:16 -0300

    fix: corrigindo erro de sintaxe no tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config: Config {
+const config: Config = {
   // ATENÇÃO: Esta linha é essencial para o funcionamento do Modo Escuro via botão
   darkMode: 'class',
   
